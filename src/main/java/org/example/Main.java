26ab3df59f329ba849eca9a222a720d0c369c95c
Alexander <j32lemutov@gmail.com>
2023-12-09 01:50:45 +0300

    example java code finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,49 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a = 5;
+        float b = 1000.5F; //4 байта
+        char sym = 'a';
+        long l = 3_000_000_000L;
+        double c = 919.5; //8 байт
+        boolean bool = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println(a + " " + b + " " + c);
+        System.out.println(sym + " " + l);
+        System.out.println(bool);
+        String str1 = "Число: ";
+        int e = 300_000;
+        System.out.println(str1 + e);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
+
+        if ((c > 900) || (a < 1000)) {
+            System.out.println("The condition is fulfilled"); //инструмент "короткого замыкания" - не будет проверять
+                                                              //второе условие, если первое выполняется, т.е. будет работать быстрее
+        }
+        if ((c > 900) | (a < 1000)) {
+            System.out.println("The condition is fulfilled");
+        }
+
+        int nv = 10;
+        int count = 0;
+        while (nv > 5) {
+            count++;
+            nv--;
+            System.out.println(count);
+        }
+
+        int val1 = 10;
+        int val2 = 16;
+        System.out.println(calculate(val1, val2));
+        printAll(5, 5, 10);
+    }
+    public static int calculate(int a, int b){
+        return (a+b)/2;
+    }
+
+    public static void printAll(int a, int b, int c){
+        System.out.println(a + " " + b + " " + c);
     }
 }
